cff74d804a0075b22693c75e8fbc26fdb47f2271
Necker <wojtek.drzewicki@gmail.com>
2020-01-05 17:40:49 +0100

    changing media type

--- src/main/java/com/necker/example/task5/controller/FooController.java
+++ src/main/java/com/necker/example/task5/controller/FooController.java
@@ -3,7 +3,9 @@ package com.necker.example.task5.controller;
 import com.necker.example.task5.model.Foo;
 import com.necker.example.task5.service.FooService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -21,7 +23,7 @@ public class FooController {
         this.fooService = fooService;
     }
 
-    @GetMapping("/foos")
+    @RequestMapping(value = "/foos", method = RequestMethod.GET, produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Foo> getFoos() {
         return fooService.findAll().delayElements(Duration.ofSeconds(1));
     }
